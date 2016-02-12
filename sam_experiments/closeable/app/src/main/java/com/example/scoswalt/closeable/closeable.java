18eb9e3f8e9a79f0f68ffab827c052b9e0c16791
unknown <scoswalt@CO2042-15.ece.iastate.edu>
2016-02-12 11:50:57 -0600

    closeable improvements

--- sam_experiments/closeable/app/src/main/java/com/example/scoswalt/closeable/closeable.java
+++ sam_experiments/closeable/app/src/main/java/com/example/scoswalt/closeable/closeable.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.PopupWindow;
 
 public class closeable extends AppCompatActivity {
 
@@ -11,8 +15,26 @@ public class closeable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_closeable);
-    }
+        final Button button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
 
+        });
+        final PopupWindow popup = new PopupWindow(50,50);
+        final Button button2 = (Button) findViewById(R.id.btn2);
+        button2.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                popup.showAtLocation(findViewById(R.id.android), 1, 0, 0);
+            }
+
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
