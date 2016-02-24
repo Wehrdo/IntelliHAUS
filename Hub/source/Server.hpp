5640994f3711f7174e067fee25b5437680c3121a
EricMiddleton1 <ericm@iastate.edu>
2016-02-24 19:11:02 +0000

    Updated Server api

--- Hub/source/Server.hpp
+++ Hub/source/Server.hpp
@@ -18,9 +18,22 @@ public:
 
 	bool IsConnected();
 
+	//Interface for server REST API
+	int Authenticate(const string& username, const string& password);
+
+	int CreateUser(const string& username, const string& password,
+			const string& firstName, const string& lastName);
+
+	string& ListNodes(int homeID);
+	int CreateNode(int home, const string& inTypes, const string& inNames,
+			const string& outTypes, const string& outNames);
+
+	int CreateHome(const string& homeName);
 
 	int CreateDatastream(string name, vector<string> ioTypes);
-	int SendDatastream(string name, string data);
+	int SendDatapoint(string name, string data);
+
+	
 
 private:
 	std::unique_ptr<HTTP> httpREST;
