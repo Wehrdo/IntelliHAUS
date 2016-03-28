f6a96d403c8dcc294f67d73aae1f0a454154a5d1
scoswalt <scoswalt@iastate.edu>
2016-03-28 09:30:24 -0500

    d3 first experiment

---
+++ d3_projects/onload.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() { 
+    // this function runs when the DOM is ready, i.e. when the document has been parsed
+    
+	d3.select('body').append('p');
+	
+});
