9edce3f8ab00a339f07bd9c5dbb46270140cb851
scoswalt <scoswalt@iastate.edu>
2016-04-13 10:31:03 -0500

    Calculate height, edit tree object with height data

--- public/js/rule_ui/graphics.js
+++ public/js/rule_ui/graphics.js
@@ -6,6 +6,21 @@ function RuleGraphics() {
     /*
     Public methods
      */
+    function setHeights(nodeData, id) {
+	var height=0;
+	if(nodeData[id.toString()].branches==[])
+		height=2;
+	else
+	{
+		var i;
+		for(i=0;i<nodeData[id.toString()].branches.length;i++)
+		{
+			height+=setHeights(nodeData, nodeData[id.toString()].branches[i])[branches[i].toString()].height;
+		}
+	}
+	nodeData[id.toString()].height=height;
+	return nodeData;
+    }
     this.updateTree = function() {
 
     };
