a683531051aa323e5732cb19383f123aa0d27c85
scoswalt <scoswalt@iastate.edu>
2016-04-13 10:36:20 -0500

    Added support for empty branches. Can be adjusted later.

--- public/js/rule_ui/rule_container.js
+++ public/js/rule_ui/rule_container.js
@@ -54,70 +54,71 @@ function RuleContainer() {
 	//use pid=null for root node
 	//setting id=0 starts effectual id count at 1
 	function translate(tree, pid){
-		var newObj={};
-		var temp={};
-		var branchPush={};
-		var rangePush;
-		id++;
-		var dotId=id,
-			type=Object.keys(tree)[0],
-			branches=[],
-			ranges=[],
-			nodeId=null,
-			data=null,
-			datastreamId=null,
-			parentId=pid;
-		if(type=='NodeInput')
+	var newObj={};
+	var temp={};
+	var branchPush={};
+	var rangePush;
+	id++;
+	var dotId=id,
+		type=Object.keys(tree)[0],
+		branches=[],
+		ranges=[],
+		nodeId=null,
+		data=null,
+		datastreamId=null,
+		parentId=pid;
+	if(type=='NodeInput')
+	{
+		data=tree[type].data;
+		nodeId=tree[type].nodeId;
+	}
+	else
+	{
+		if(tree[type].datastreamId)
+			datastreamId=tree[type].datastreamId;
+		if(tree[type].branches.length)
 		{
-			data=tree[type].data;
-			nodeId=tree[type].nodeId;
-		}
-		else
-		{
-			parentId=pid;
-			if(tree[type].datastreamId)
-				datastreamId=tree[type].datastreamId;
-			if(tree[type].branches.length)
+			var i;
+			for(i=0;i<tree[type].branches.length;i++)
 			{
-				var i;
-				for(i=0;i<tree[type].branches.length;i++)
-				{
-					branchPush=translate(tree[type].branches[i].action, dotId);
-					for(var key in branchPush)
-						temp[key]=branchPush[key];
-					rangePush=tree[type].branches[i].value;
-					if(!rangePush.length)
-						rangePush=[rangePush, null];
-					branches.push(Object.keys(branchPush)[0]);
-					ranges.push(rangePush);
-				}
-			}
-			else
-			{
-				branchPush=translate(tree[type].branches.action, dotId);
-				rangePush=tree[type].branches.value;
+				branchPush=translate(tree[type].branches[i].action, dotId);
+				for(var key in branchPush)
+					temp[key]=branchPush[key];
+				rangePush=tree[type].branches[i].value;
 				if(!rangePush.length)
 					rangePush=[rangePush, null];
 				branches.push(Object.keys(branchPush)[0]);
 				ranges.push(rangePush);
 			}
-				tree[type].branches[key]
 		}
-		newObj[dotId.toString()]={
-			"dotId" : dotId,
-			"type" : type,
-			"parent" : parentId,
-			"branches" : branches,
-			"ranges" : ranges,
-			"nodeId" : nodeId,
-			"data" : data,
-			"datastreamId" : datastreamId};
-		for(var key in temp)
+		else if(tree[type].branches.action)
 		{
-			newObj[key]=temp[key];
+			branchPush=translate(tree[type].branches.action, dotId);
+			rangePush=tree[type].branches.value;
+			if(!rangePush.length)
+				rangePush=[rangePush, null];
+			branches.push(Object.keys(branchPush)[0]);
+			ranges.push(rangePush);
 		}
-		return newObj;
+		else
+		{
+			
+		}
 	}
+	newObj[dotId.toString()]={
+		"dotId" : dotId,
+		"type" : type,
+		"parent" : parentId,
+		"branches" : branches,
+		"ranges" : ranges,
+		"nodeId" : nodeId,
+		"data" : data,
+		"dataStreamId" : datastreamId};
+	for(var key in temp)
+	{
+		newObj[key]=temp[key];
+	}
+	return newObj;
 }
 
 window.ruleContainer = new RuleContainer();
